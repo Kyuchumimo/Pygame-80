bd33c7d5e279210463b08a60c90facb21979a778
Kyuchumimo <74131798+Kyuchumimo@users.noreply.github.com>
2024-09-28 22:33:23 -0700

    Update pygame80 version string @ pygame.display.set_caption

--- pygame80.py
+++ pygame80.py
@@ -20,7 +20,7 @@ pygame.init()
 ###PARAMETERS###
 # WINDOW
 pygame.display.set_icon(pygame.image.load(os.path.join(_ASSET_PATH, 'icon.png')))
-pygame.display.set_caption("Pygame-80 by Kyuchumimo v230807")
+pygame.display.set_caption("Pygame-80 by Kyuchumimo v240928")
 _SCREEN = pygame.display.set_mode([240, 136], pygame.SCALED)
 
 # MUSIC CHANNELS
