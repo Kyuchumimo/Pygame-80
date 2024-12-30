264e180c373ebeafe9cb449362e14460b4992b88
Kyuchumimo <74131798+Kyuchumimo@users.noreply.github.com>
2024-12-30 16:46:11 -0700

    Update key and keyp function for no parameters behavior

--- pygame80.py
+++ pygame80.py
@@ -253,10 +253,10 @@ def key(*args):
     Description:
             The function returns True if the key denoted by keycode is pressed.
     """
-    keycodes = (False, pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f, pygame.K_g, pygame.K_h, pygame.K_i, pygame.K_j, pygame.K_k, pygame.K_l, pygame.K_m, pygame.K_n, pygame.K_o, pygame.K_p, pygame.K_q, pygame.K_r, pygame.K_s, pygame.K_t, pygame.K_u, pygame.K_v, pygame.K_w, pygame.K_x, pygame.K_y, pygame.K_z, pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9, pygame.K_MINUS, pygame.K_EQUALS, pygame.K_LEFTBRACKET, pygame.K_RIGHTBRACKET, pygame.K_BACKSLASH, pygame.K_SEMICOLON, None, pygame.K_BACKQUOTE, pygame.K_COMMA, pygame.K_PERIOD, pygame.K_SLASH, pygame.K_SPACE, pygame.K_TAB, pygame.K_RETURN, pygame.K_BACKSPACE, pygame.K_DELETE, pygame.K_INSERT, pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_HOME, pygame.K_END, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_CAPSLOCK, pygame.KMOD_CTRL, pygame.KMOD_SHIFT, pygame.K_LALT)
+    keycodes = (False, pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f, pygame.K_g, pygame.K_h, pygame.K_i, pygame.K_j, pygame.K_k, pygame.K_l, pygame.K_m, pygame.K_n, pygame.K_o, pygame.K_p, pygame.K_q, pygame.K_r, pygame.K_s, pygame.K_t, pygame.K_u, pygame.K_v, pygame.K_w, pygame.K_x, pygame.K_y, pygame.K_z, pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9, pygame.K_MINUS, pygame.K_EQUALS, pygame.K_LEFTBRACKET, pygame.K_RIGHTBRACKET, pygame.K_BACKSLASH, pygame.K_SEMICOLON, False, pygame.K_BACKQUOTE, pygame.K_COMMA, pygame.K_PERIOD, pygame.K_SLASH, pygame.K_SPACE, pygame.K_TAB, pygame.K_RETURN, pygame.K_BACKSPACE, pygame.K_DELETE, pygame.K_INSERT, pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_HOME, pygame.K_END, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_CAPSLOCK, pygame.KMOD_CTRL, pygame.KMOD_SHIFT, pygame.K_LALT)
     
     if len(args) == 0:
-        return pygame.key.get_pressed().count(True) > 0
+        return any(pygame.key.get_pressed()[i] for i in keycodes)
     elif len(args) == 1:
         try:
             code = int(args[0])
@@ -281,10 +281,10 @@ def keyp(*args):
     Description:
             This function returns True if the given key is pressed but wasn't pressed in the previous frame.
     """
-    keycodes = (False, pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f, pygame.K_g, pygame.K_h, pygame.K_i, pygame.K_j, pygame.K_k, pygame.K_l, pygame.K_m, pygame.K_n, pygame.K_o, pygame.K_p, pygame.K_q, pygame.K_r, pygame.K_s, pygame.K_t, pygame.K_u, pygame.K_v, pygame.K_w, pygame.K_x, pygame.K_y, pygame.K_z, pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9, pygame.K_MINUS, pygame.K_EQUALS, pygame.K_LEFTBRACKET, pygame.K_RIGHTBRACKET, pygame.K_BACKSLASH, pygame.K_SEMICOLON, None, pygame.K_BACKQUOTE, pygame.K_COMMA, pygame.K_PERIOD, pygame.K_SLASH, pygame.K_SPACE, pygame.K_TAB, pygame.K_RETURN, pygame.K_BACKSPACE, pygame.K_DELETE, pygame.K_INSERT, pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_HOME, pygame.K_END, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_CAPSLOCK, pygame.KMOD_CTRL, pygame.KMOD_SHIFT, pygame.K_LALT)
+    keycodes = (False, pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f, pygame.K_g, pygame.K_h, pygame.K_i, pygame.K_j, pygame.K_k, pygame.K_l, pygame.K_m, pygame.K_n, pygame.K_o, pygame.K_p, pygame.K_q, pygame.K_r, pygame.K_s, pygame.K_t, pygame.K_u, pygame.K_v, pygame.K_w, pygame.K_x, pygame.K_y, pygame.K_z, pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9, pygame.K_MINUS, pygame.K_EQUALS, pygame.K_LEFTBRACKET, pygame.K_RIGHTBRACKET, pygame.K_BACKSLASH, pygame.K_SEMICOLON, False, pygame.K_BACKQUOTE, pygame.K_COMMA, pygame.K_PERIOD, pygame.K_SLASH, pygame.K_SPACE, pygame.K_TAB, pygame.K_RETURN, pygame.K_BACKSPACE, pygame.K_DELETE, pygame.K_INSERT, pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_HOME, pygame.K_END, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_CAPSLOCK, pygame.KMOD_CTRL, pygame.KMOD_SHIFT, pygame.K_LALT)
     
     if len(args) == 0:
-        return pygame.key.get_just_pressed().count(True) > 0
+        return any(pygame.key.get_just_pressed()[i] for i in keycodes)
     elif len(args) == 1:
         try:
             code = int(args[0])
